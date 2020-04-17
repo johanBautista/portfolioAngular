fdaf5a6f71f8732606e961ef0b309d3117b83d03
johan Bautista <jsbparra@gmail.com>
2020-04-17 15:51:11 +0200

    wip serviceas and interfaces

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -5,7 +5,17 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class InfoPaginaService {
+
+  info: any ={};
+  cargada = false;
+  
   constructor(private http: HttpClient) {
     console.log('lolo');
+
+    this.http.get('assets/data/data-pagina.json').subscribe((resp) => {
+      this.cargada = true;
+      this.info = resp;
+      console.log(resp);
+    });
   }
 }
