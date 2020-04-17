14b60444b5fdb1224bf489d5e808d63bd5b57955
johan Bautista <jsbparra@gmail.com>
2020-04-17 19:37:41 +0200

    add firebase and service about, pend-links

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interfaces';
+// import { InfoEquipo } from '../interfaces/info-equipo.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,34 @@ export class InfoPaginaService {
   info: InfoPagina ={};
   cargada = false;
 
-  constructor(private http: HttpClient) {
-    console.log('lolo');
+  equipo: any[] = [];
+  // equipo: InfoEquipo = {};
 
+  constructor(private http: HttpClient) {
+    // console.log('lolo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json').subscribe((resp:InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
+    this.cargada = true;
+    this.info = resp;
+    // console.log(resp);
+    });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://dev-johan.firebaseio.com/equipo.json').subscribe((resp: any[]) => {
+      this.equipo = resp;
       console.log(resp);
     });
   }
+
+  
 }
+
+
+
+
+
